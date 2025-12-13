01ad104ea5b0600ba3e9c1fde73506df118d66b0
Yandelf00 <zakaria.afyf00@gmail.com>
2025-12-13 14:56:22 +0100

    lpush

--- mthsht.c
+++ mthsht.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void match(char c, char **pt);
+void e_prime(char **pt);
+
+void e(char **pt){
+	if(**pt == 'i'){
+		match('i', pt);
+		e_prime(pt);
+	} else {
+		printf("there was a error, expected i");
+	}
+}
+
+void e_prime(char **pt){
+	if(**pt == '+'){
+		match('+', pt);
+		match('i', pt);
+		e_prime(pt);
+	}else {
+		return;
+	} 
+}
+
+void match(char c, char **pt){
+	if(**pt == c){
+		(*pt)++;
+	} else {
+		printf("expected something else");
+	}
+}
+
 int main(void) {
-    printf("Starting program...\n");
-
-    // Allocate memory but never free it (leak)
-    char *leak = malloc(100);
-
-    // Write something into it so the compiler doesn’t optimize it away
-    for (int i = 0; i < 99; i++) {
-        leak[i] = 'A';
-    }
-    leak[99] = '\0';
-
-    printf("Allocated string: %s\n", leak);
-
-    // Intentionally forget to free(leak);
-    // free(leak);  <-- if you uncomment this, leak disappears
-
-    return 0;
+	char test[] = "i+i+i+i+i";
+	char *pt = test;
+	e(&pt);
+	if(*pt == '\0'){
+		printf("Valid expression\n");
+	} else {
+		printf("it didn't work\n");
+	}
+  return 0;
 }
