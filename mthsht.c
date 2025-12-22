efd82efd0c1fafdbef433436270506926998e9cf
Yandelf00 <zakaria.afyf00@gmail.com>
2025-12-22 23:48:34 +0000

    some revision of simple parser

--- mthsht.c
+++ mthsht.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "mthsht.h"
+
+void e(char **el)
+{
+  if(**el == 'i'){
+    match(**el, 'i');
+    (*el)++;
+    e_prime(**el);
+  }
+}
+
+void e_prime(char **el){
+
+}
 
 int main(void) {
-    printf("Starting program...\n");
+  char text[] = "i+i";
+  char *ptr = &text[0];
+  e(&ptr);
 
-    // Allocate memory but never free it (leak)
-    char *leak = malloc(100);
-
-    // Write something into it so the compiler doesn’t optimize it away
-    for (int i = 0; i < 99; i++) {
-        leak[i] = 'A';
-    }
-    leak[99] = '\0';
-
-    printf("Allocated string: %s\n", leak);
-
-    // Intentionally forget to free(leak);
-    // free(leak);  <-- if you uncomment this, leak disappears
-
-    return 0;
+  return 0;
 }
---
+++ mthsht.h
@@ -0,0 +1,10 @@
+#ifndef __MTSHT__
+#define __MTSHT__
+#include <stdlib.h>
+#include <stdio.h>
+
+void e(char **el);
+void e_prime(char **el);
+void match(char **el, char c);
+
+#endif
